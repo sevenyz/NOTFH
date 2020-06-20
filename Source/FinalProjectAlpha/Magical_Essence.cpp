3de846c424d050d74b06f14ad3a4144e607cdf13
marcopompa <marcopompa7@gmail.com>
2020-06-20 13:55:02 +0200

    Removed rotation on Magical Essence

--- Source/FinalProjectAlpha/Magical_Essence.cpp
+++ Source/FinalProjectAlpha/Magical_Essence.cpp
@@ -45,7 +45,7 @@ void AMagical_Essence::BeginPlay()
 void AMagical_Essence::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	MoveObject();
+	//MoveObject();
 }
 
 void AMagical_Essence::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
